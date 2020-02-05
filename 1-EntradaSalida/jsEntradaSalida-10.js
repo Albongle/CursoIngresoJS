c0b1efccfe23db1946c205d1c2c8ea7204537237
albongle <alejandro.bongioanni@gmail.com>
2020-02-05 20:58:44 -0300

    comentario

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -8,5 +8,5 @@ function mostrarAumento()
     var resultado;
 	importe=parseFloat(document.getElementById("importe").value);
     resultado=importe - (importe*25/100);
-    document.getElementById("resultado").value=resultado;
+    document.getElementById("resultado").value=resultado.toFixed(2); //Para redondear
 }
