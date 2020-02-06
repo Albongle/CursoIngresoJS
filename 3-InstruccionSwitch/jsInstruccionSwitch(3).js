936a58202f14574841e47bd46ecef5bdd3e95663
Alejandro Bongioanni <60529636+Albongle@users.noreply.github.com>
2020-02-06 12:02:43 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,51 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    var mesDelAño = document.getElementById('mes').value;
+    //var invierno;
+    //var antesinv;
+    //var despinv;
+    //invierno=(mesDelAño=="Julio") || (mesDelAño=="Agosto");
+    //antesinv=(mesDelAño=="Enero") || (mesDelAño=="Febrero")|| (mesDelAño=="Marzo")|| (mesDelAño=="Abril")|| (mesDelAño=="Mayo")
+    //|| (mesDelAño=="Junio");
+    //despinv=(mesDelAño=="Septiembre") || (mesDelAño=="Octubre")|| (mesDelAño=="Noviembre")|| (mesDelAño=="Diciembre");
+    
+    switch (mesDelAño)
+    {
+    case "Febrero" :
+       
+      {
+        alert("Este mes no tiene más de 29 días");
+        break;
+      }
+    
+    case "Enero" :
+    case "Marzo" :
+    case "Abril" :
+    case "Mayo" :
+    case "Junio" :
+    case "Julio" :
+    case "Agosto" :
+    case "Septiembre" :
+    case "Octubre" :
+    case "Noviembre" :
+    case "Diciembre" :
 
-//alert (mesDelAño);
-	
-	
+      {
+        alert("Este mes tiene 30 o más días");
+        break;
+      }
+    
+      
+    
+    
+    
+    
+    
+    }
 
 
-}//FIN DE LA FUNCIÓN
+
+
+}
+
+
