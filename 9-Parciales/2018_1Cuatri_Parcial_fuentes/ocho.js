d5f0f189ec8594494c7839792f196d472dd02330
Alejandro N Bongioanni <ANBongioanni@teco.com.ar>
2020-02-10 16:05:36 -0300

    Parciales

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,121 @@
 function mostrar()
 {
+var x=1;
+var ingLetra;
+var ingNum;
+var num1= new Array();
+var letra1 =new Array();
+var pares=0;
+var contpares=0;
+var continpares=0;
+var Qceros=0;
+var Qpos=0;
+var Qneg=0;
+var SumPos=0;
+var SumNeg=0;
+var prompos;
+var n_max=0;
+var n_min=0;
+var l_max;
+var l_min;
+var prom=0;
+
+
+while (ingLetra != "Informar" && ingLetra != "informar")
+{
+    
+    ingLetra=prompt("Ingrese la letra numero " + x  + " para informar ingrese: " , "Informar");
+
+
+
+
+    if (ingLetra != "Informar" && ingLetra != "informar")
+    {
+
+    letra1[x]=ingLetra;
+
+    ingNum=parseInt(prompt("Ingrese un numero " + x  + " :" , "-100 a 100"));
+
+    while (ingNum < -100 || ingNum >100) 
+        {
+            alert("Verifique el numero ingresado");
+            ingNum=parseInt(prompt("Ingrese un numero " + x  + " :" , "-100 a 100"));
+        
+        }
+
+        num1[x]=ingNum;
+        
+        x++;  
+    }
+
+
+}
+
+
+for (y=1; y<x; y++)
+{
+    //numeros pares
+pares=num1[y] % 2;
+    if (pares<1)
+    {
+        contpares++;
+    }
+    else
+    {
+        continpares++;
+    }
+//sumo positivos y tot
+    if (num1[y]>0)
+        {
+       SumPos=num1[y]+SumPos;
+       Qpos++;
+       
+        }
+//sumo negativos y tot
+    else if (num1[y]<0)
+        {
+
+            SumNeg=num1[y]+SumNeg;
+            Qneg++;
+            
+        } 
+// cuento ceros y y tot
+    else
+        {
+            Qceros++; 
+            
+        }
+// maximos y minimos
+    if (num1[y] >= n_max)
+        {
+            n_max=num1[y];
+            l_max=letra1[y];
+        }
+    else if (num1[y] <= n_min)
+        {
+            n_min=num1[y];
+            l_min=letra1[y];
+        }
+        
+
+
+}
+
+
+prompos=SumPos/Qpos;
+//a
+document.write("La cantidad de números pares es: " + contpares);
+//b
+document.write(" La cantidad de números impares es: " + continpares);
+//c
+document.write(" La cantidad de ceros es: " + Qceros);
+//d
+document.write(" El promedio de todos los numeros POS ingresados es: " + prompos);
+//e
+document.write(" La suma total de los negativos es: " + SumNeg);
+//f
+document.write(" El numero Max es: " + n_max + " Su letra es " + l_max);
+document.write(" El numero Min es: " + n_min + " Su letra es " + l_min);
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,72 @@
 function mostrar()
 {
+    var nota = new Array();
+    var n=22;
+    var s="p";
+    var sex= new Array();
+    var x=1;
+    var promtotal=0;
+    var NotaBaja;
+    var sexnotabaja;
+    var cont=0;
+
+    while (x<6)
+    {
+
+        while (n< 0 || n>= 11)
+        {
+
+            n=parseInt(prompt("Ingrese la nota del Alumno " + x  + " :" , "0 a 10"));
+            if (n< 0 || n>= 11) 
+                {
+                    alert("Verifique la nota ingresada");
+                
+                }
+
+
+        }
+         
+        nota[x]=n;
+        while ((s != "F" && s != "f") && (s != "M" && s != "m"))
+        {
+
+            s=prompt("Ingrese el sexo del Alumno " + x  + " :", "M o F");
+            if ((s != "F" && s != "f") && (s != "M" && s != "m")) 
+                {
+                    alert("Verifique el sexo ingresado");
+                
+                }
+        
+
+        }
+        if (n<nota[x-1])
+        {
+            NotaBaja=n;
+            sexnotabaja=s;
+        } 
+
+        sex[x]=s;
+        x++;
+        s="";
+        n=22;
+
+        
+    }
+
+    for (y=1; y<6; y++)
+        {
+            promtotal=nota[y]+promtotal;
+            if (nota[y]>=6 && (sex[y]=="M" ||sex[y]=="m"))
+            {
+                cont=cont+1;
+            }
+
+
+        }
+
+    promtotal=promtotal/5;
+alert("El promedio de las notas totales es: " + promtotal );
+alert("La nota mas baja fue: " + NotaBaja + " bajo el sexo " + sexnotabaja );
+alert("La candtidad de varones con nota >6 son: " + cont );
 
 }
