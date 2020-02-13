5b41630f9f6dbdb872996a41caf0f8e2d624f576
Albongle <alejandro.bongioanni@gmail.com>
2020-02-12 22:14:04 -0300

    Cambio

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -3,7 +3,7 @@ function mostrar()
 
 
 var numero = parseInt(prompt("ingrese un número entre 0 y 9."));
-while (numero<0 || numero >9)
+while (numero<0 || numero >9 || isNaN(numero))
 {
 	alert("Verifique el numero ingresado");
 	numero = parseInt(prompt("ingrese un número entre 0 y 9."));
