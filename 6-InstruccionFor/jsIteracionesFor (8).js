28471e1b983ccdb1a95ce883a01ed7fa447f700a
Alejandro Bongioanni <60529636+Albongle@users.noreply.github.com>
2020-02-26 21:18:44 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,33 @@
 function mostrar()
 {
 
-
-
+    var repetciones = parseInt(prompt("ingrese el número de repeticiones"));
+    var qnumeros=0;
+    while(isNaN(repetciones))
+    {
+        alert("Verifique el numero Ingresado");
+        repetciones = parseInt(prompt("ingrese el número de repeticiones"));
+    }
+    
+    for (cont=1;cont<=repetciones;cont++)
+    {
+    
+    if ((repetciones%cont)==0)
+        {
+        
+            document.write('Numero ' + cont + "</br>");
+            qnumeros++;
+    
+        
+        }
+    }
+    if (qnumeros>2)
+    {
+    document.write('No es primo ya que es divisible por ' + qnumeros + " numeros </br>");
+    }
+    else
+    {
+    document.write("El numero es primo </br>");
+    }
 
 }//FIN DE LA FUNCIÓN
