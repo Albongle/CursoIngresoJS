6a890f07cb8d4b2695be4cd64b2820b1604bb469
Alejandro Bongioanni <60529636+Albongle@users.noreply.github.com>
2020-02-26 21:00:52 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,22 +1,26 @@
 function mostrar()
 {
-
+    document.write("<body bgcolor='red'><font color='white'><center>");
     var repetciones = parseInt(prompt("ingrese el número de repeticiones"));
+    var qpares=0;
     while(isNaN(repetciones))
     {
         alert("Verifique el numero Ingresado");
         repetciones = parseInt(prompt("ingrese el número de repeticiones"));
     }
     
-    for (cont=0;cont<repetciones;cont++)
+    for (cont=1;cont<=repetciones;cont++)
     {
     
     if ((cont%2)==0)
         {
+        qpares++;
         document.write('Numero ' + cont + "</br>");
     
         
         }
     }
+    document.write('Cantidad de numeros pares: ' + qpares + "</br>");
+    
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,26 @@
 function mostrar()
 {
 
-
-
+    var repetciones = parseInt(prompt("ingrese el número de repeticiones"));
+    var qnumeros=0;
+    while(isNaN(repetciones))
+    {
+        alert("Verifique el numero Ingresado");
+        repetciones = parseInt(prompt("ingrese el número de repeticiones"));
+    }
+    
+    for (cont=1;cont<=repetciones;cont++)
+    {
+    
+    if ((repetciones%cont)==0)
+        {
+        
+            document.write('Numero ' + cont + "</br>");
+            qnumeros++;
+    
+        
+        }
+    }
+    document.write('Cantidad de numeros divisibles: ' + qnumeros + "</br>");
 
 }//FIN DE LA FUNCIÓN
