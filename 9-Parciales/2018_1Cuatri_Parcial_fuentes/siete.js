a9a82fb5288a9f8a79d953cb2aaf6819a1df0bd1
Alejandro Bongioanni <60529636+Albongle@users.noreply.github.com>
2020-02-26 22:01:45 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,14 +1,13 @@
 function mostrar()
 {
-    var nota_a = new Array();
     var nota;
     var sexo;
-    var sexo_a= new Array();
     var x_incremento=1;
     var promtotal=0;
     var NotaBaja;
+    var acumnota=0;
     var sexnotabaja;
-    var cont=0;
+    var contvarones=0;
 
     for (x_incremento=1; x_incremento<6;x_incremento++)
     {
@@ -21,7 +20,6 @@ function mostrar()
             
         }
          
-        nota_a[x_incremento]=nota;
 
         sexo=prompt("Ingrese el sexo del Alumno " + x_incremento  + " :", "M o F");
         while ((sexo != "F" && sexo != "f") && (sexo != "M" && sexo != "m"))
@@ -30,33 +28,36 @@ function mostrar()
             sexo=prompt("Ingrese el sexo del Alumno " + x_incremento  + " :", "M o F");
 
         }
-        sexo_a[x_incremento]=sexo;
-        
-        if (nota<nota_a[x_incremento-1])
+
+        if (x_incremento==1)
         {
             NotaBaja=nota;
             sexnotabaja=sexo;
-        } 
+        }
+        else if (nota<NotaBaja)
+        {
+            NotaBaja=nota;
+            sexnotabaja=sexo;
+        }
+
+        if (nota>=6 && sexo=="M" || nota>=6 && sexo=="m" )
+        {
+            contvarones++;
+        }
+
+        
+      acumnota=acumnota+nota;  
+        
 
        
         
                
     }
 
-    for (y=1; y<6; y++)
-        {
-            promtotal=nota_a[y]+promtotal;
-            if (nota_a[y]>=6 && (sexo_a[y]=="M" ||sexo_a[y]=="m"))
-            {
-                cont=cont+1;
-            }
-
-
-        }
-
-    promtotal=promtotal/5;
-alert("El promedio de las notas totales es: " + promtotal );
+   
+promtotal=acumnota/5;
+alert("El promedio de las notas totales es: " + promtotal  );
 alert("La nota mas baja fue: " + NotaBaja + " bajo el sexo " + sexnotabaja );
-alert("La candtidad de varones con nota >6 son: " + cont );
+alert("La candtidad de varones con nota >6 son: " + contvarones );
 
 }
